0cbf427f403d6c23f3783c4e5215ace14bfbd8fc
ricsanfre <ricsanfre@gmail.com>
2022-12-14 14:56:39 +0100

    linkerd makefile

--- linkerd/deploy_traefik.sh
+++
@@ -1,4 +0,0 @@
-kubectl create namespace traefik
-helm repo add traefik https://helm.traefik.io/traefik
-helm repo update
-helm install traefik traefik/traefik --create-namespace -n traefik -f traefik-values.yml
