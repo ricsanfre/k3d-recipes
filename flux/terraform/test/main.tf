85b7c87850468912adffab3b4f907a9f5af976a0
Ricardo Sanchez <ricsanfre@gmail.com>
2025-12-26 10:38:05 +0100

    Updating random provider

--- flux/terraform/test/main.tf
+++ flux/terraform/test/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0.0"
   required_providers {
     random = {
-      source  = "opentofu/random"
+      source  = "hashicorp/random"
       version = "3.7.2"
     }
     kubernetes = {
