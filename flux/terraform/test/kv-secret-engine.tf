c21cf0283ef4a0a52655f78e9290afaa7fdc67e5
Ricardo Sanchez <ricsanfre@gmail.com>
2025-12-27 12:17:34 +0100

    Adding create token policy

--- flux/terraform/test/kv-secret-engine.tf
+++ flux/terraform/test/kv-secret-engine.tf
@@ -11,34 +11,14 @@ resource "vault_mount" "kv_engine_v2" {
   description = "KV Version 2 secret engine mount"
 }
 
-# Create access policies
-# Read-only
-resource "vault_policy" "readonly" {
-  name   = "readonly"
-  policy = <<-EOT
-  path "secret/*" {
-    capabilities = ["read", "list"]
-  }
-  EOT
-}
-
-# Write
-resource "vault_policy" "readwrite" {
-  name   = "readwrite"
-  policy = <<-EOT
-   path "secret/*" {
-     capabilities = ["create", "read", "update", "delete", "list", "patch"]
-   }
-   EOT
-}
 
 # Create secret in KV Version 2 Secret Engine
 
 resource "vault_kv_secret_v2" "secrets" {
-  for_each = local.secrets_data
+  for_each            = local.secrets_data
   mount               = vault_mount.kv_engine_v2.path
   name                = each.key
   cas                 = 1
   delete_all_versions = true
-  data_json = jsonencode(each.value.content)
+  data_json           = jsonencode(each.value.content)
 }
---
+++ flux/terraform/test/policies.tf
@@ -0,0 +1,30 @@
+# Create KV2 access policies
+# Read-only
+resource "vault_policy" "readonly" {
+  name   = "readonly"
+  policy = <<-EOT
+  path "secret/*" {
+    capabilities = ["read", "list"]
+  }
+  EOT
+}
+
+# Write
+resource "vault_policy" "readwrite" {
+  name   = "readwrite"
+  policy = <<-EOT
+   path "secret/*" {
+     capabilities = ["create", "read", "update", "delete", "list", "patch"]
+   }
+   EOT
+}
+
+# Token management
+resource "vault_policy" "create-token" {
+  name   = "create-token"
+  policy = <<-EOT
+    path "auth/token/create" {
+        capabilities = [ "create", "read", "update", "delete", "list" ]
+    }
+    EOT
+}
